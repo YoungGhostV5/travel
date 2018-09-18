77658539db2f9f427938886aef8d8063dd389416
jing <862661612@qq.com>
2018-09-18 10:48:09 +0800

    ajax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -12,62 +12,15 @@
 			<div class="area">
 				<div class="title border-topbottom">热门城市</div>
 				<div class="button-list">
-					<div class="button-wrapper">
-						<div class="button">北京</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">北京</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">北京</div>
-					</div>
-					<div class="button-wrapper">
-						<div class="button">北京</div>
+					<div class="button-wrapper" v-for="item of hotCity" :key="item.id">
+						<div class="button">{{item.name}}</div>
 					</div>
 				</div>
 			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">北京</div>
-					<div class="item border-bottom">北京</div>
-					<div class="item border-bottom">北京</div>
-					<div class="item border-bottom">北京</div>
-					<div class="item border-bottom">北京</div>
-					<div class="item border-bottom">北京</div>
-				</div>
-			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">上海</div>
-					<div class="item border-bottom">上海</div>
-					<div class="item border-bottom">上海</div>
-					<div class="item border-bottom">上海</div>
-					<div class="item border-bottom">上海</div>
-					<div class="item border-bottom">上海</div>
-				</div>
-			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">深圳</div>
-					<div class="item border-bottom">深圳</div>
-					<div class="item border-bottom">深圳</div>
-					<div class="item border-bottom">深圳</div>
-					<div class="item border-bottom">深圳</div>
-					<div class="item border-bottom">深圳</div>
-				</div>
-			</div>
-			<div class="area">
-				<div class="title border-topbottom">A</div>
-				<div class="item-list">
-					<div class="item border-bottom">123</div>
-					<div class="item border-bottom">123</div>
-					<div class="item border-bottom">123</div>
-					<div class="item border-bottom">123</div>
-					<div class="item border-bottom">123</div>
-					<div class="item border-bottom">123</div>
+			<div class="area" v-for="(item, key) of city" :key="key">
+				<div class="title border-topbottom">{{key}}</div>
+				<div class="item-list" v-for="innerItem of item" :key="innerItem.id">
+					<div class="item border-bottom">{{innerItem.name}}</div>
 				</div>
 			</div>
 		</div>
@@ -79,6 +32,10 @@ import Bscroll from 'better-scroll';
 
 export default {
   name: 'CityList',
+  props: {
+  	city: Object,
+  	hotCity: Array
+  },
   mounted() {
   	this.scroll = new Bscroll(this.$refs.wrapper);
   }
