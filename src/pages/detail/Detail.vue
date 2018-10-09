13f3438314092af181d4965881d3b0a1a866c610
jing <862661612@qq.com>
2018-10-09 16:55:43 +0800

    123

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -9,12 +9,43 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import axios from 'axios'
 
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
         DetailHeader
+    },
+    data() {
+        return {
+            sightName: '',
+            bannerImg: '',
+            gallaryImgs: [],
+            categoryList: []
+        }
+    },
+    methods: {
+        getDetailInfo() {
+            axios.get('/api/detail.json', {
+                params: {
+                    id: this.$route.params.id
+                }
+            }).then(this.handleGetDataSucc)
+        },
+        handleGetDataSucc(res) {
+            res = res.data
+            if (res.ret && res.data) {
+                const data = res.data
+                this.sightName = data.sightName
+                this.bannerImg = data.bannerImg
+                this.gallaryImgs = data.gallaryImgs
+                this.categoryList = data.categoryList
+            }
+        }
+    },
+    mounted() {
+        this.getDetailInfo()
     }
 }
 </script>
