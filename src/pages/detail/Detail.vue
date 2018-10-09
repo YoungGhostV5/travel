0b540b5122293f4971b978cd6252a25468e54521
jing <862661612@qq.com>
2018-10-09 16:12:30 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,24 +2,51 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data() {
+        return {
+            list: [{
+                title: '成人票',
+                children: [{
+                    title: '成人套餐票A',
+                    children: [{
+                        title: '成人套餐票A — 七天酒店'
+                    }, {
+                        title: '成人套餐票A — 如家酒店'
+                    }]
+                }, {
+                    title: '成人套餐票B'
+                }]
+            }, {
+                title: '学生票'
+            } ,{
+                title: '儿童票'
+            }, {
+                title: '特惠票'
+            }]
+        }
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-	.content
-		height: 50rem
+    .content
+        height: 50rem
 </style>
